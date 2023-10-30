233954ce72b3415d775cd81990810e83017d0868
sniperdev <igorspychalaa@gmail.com>
2023-10-30 22:48:59 +0100

    <feat> add routing for homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingEnum } from "./shared/utils/app.routing.enum";
+import {HomeComponent} from "./pages/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: AppRoutingEnum.home,
+    component: HomeComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
